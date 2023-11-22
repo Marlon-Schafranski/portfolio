703b0f5d1848e1e942504c0ca037c148fee027f5
Marlon-Schafranski <marlonschafranski963@gmail.com>
2023-11-22 18:23:33 -0300

    commit: contact finalizado

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -1,10 +1,15 @@
-import React from "react";
-import { useState } from "react";
-import './contact.css'
+import React, { useState } from "react";
+import './contact.css';
 
+import facebook from '../../assets/facebook.png';
+import instagram from '../../assets/instagram.png';
+import gitHub from '../../assets/github.png';
+import linkedin from '../../assets/linkedin.png';
+import whatsapp from '../../assets/whatsapp.png';
 
+import ScrollToTop from "react-scroll-to-top";
 
-
+import emailjs from '@emailjs/browser'
 
 const Contact = () => {
   const [name, setName] = useState("");
@@ -13,40 +18,102 @@ const Contact = () => {
 
   function sendEmail(e) {
     e.preventDefault();
+    if(name === '' || email === '' || message === ''){
+      alert("Preencha todos os campos")
+      return;
+    }
+   
+    const templateParams = {
+      from_name: name,
+      message: message,
+      email: email
+    }
 
-    alert("Please enter")
+    emailjs.send("service_rhcga95", "template_vcge3ob", templateParams, "S_WEDx40RrIGgdSKn" )
+    .then(( response ) => {
+      console.log("EMAIL ENVIADO", response.status, response.text)
+      setName('')
+      setEmail('')
+      setMessage('')
+
+    }, (err) => {
+      console.log("ERRO: ", err)
+    })
+    
   }
 
+  const socialMediaData = [
+    {
+      platform: 'Facebook',
+      image: facebook,
+      link: 'https://www.facebook.com/Marlon-Schafranski ',
+    },
+    {
+      platform: 'Instagram',
+      image: instagram,
+      link: 'https://www.instagram.com/marlon.schafranski_',
+    },
+    {
+      platform: 'LinkedIn',
+      image: linkedin,
+      link: 'https://www.linkedin.com/in/Marlon-Schafranski',
+    },
+    {
+      platform: 'WhatsApp',
+      image: whatsapp,
+      link: 'https://wa.me/42999560547',
+    },
+    {
+      platform: 'GitHub',
+      image: gitHub,
+      link: 'https://github.com/Marlon-Schafranski',
+    },
+  ];
+
   return (
-    <div className="container">
-      <h1 className="title">Contato</h1>
+    <div>
+      <ScrollToTop smooth top='20' />
+      <div className="container">
+        <h1 className="title">Contato</h1>
 
-      <form className="form" onSubmit={sendEmail}>
-        <input
-          className="input"
-          type="text"
-          placeholder="Digite seu nome"
-          onChange={(e) => setName(e.target.value)}
-          value={name}
-        />
+        <form className="form" onSubmit={sendEmail}>
+          <input
+            className="input"
+            type="text"
+            placeholder="Digite o seu nome..."
+            onChange={(e) => setName(e.target.value)}
+            value={name}
+          />
 
-        <input
-          className="input"
-          type="text"
-          placeholder="Digite seu email"
-          onChange={(e) => setEmail(e.target.value)}
-          value={email}
-        />
+          <input
+            className="input"
+            type="text"
+            placeholder="Digite o seu email..."
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+          />
 
-        <textarea
-          className="textarea"
-          placeholder="Digite sua mensagem..."
-          onChange={(e) => setMessage(e.target.value)}
-          value={message}
-        />
+          <textarea
+            className="textarea"
+            placeholder="Digite a sua menssagem..."
+            onChange={(e) => setMessage(e.target.value)}
+            value={message}
+          />
 
-        <input className="button" type="submit" value="Enviar" />
-      </form>
+          <input className="button" type="submit" value="Enviar" />
+        </form>
+     
+
+      <div className="social-media-container">
+        {socialMediaData.map((data, index) => (
+          <div className="social-media" key={index}>
+            <a href={data.link} target="_blank" rel="noopener noreferrer">
+              <img className="social-media-icon" src={data.image} alt={`${data.platform} icon`} />
+            </a>
+          </div>
+        ))}
+      </div>
+      </div>
     </div>
   );
 };
