c8939acc3db18261ae4fc1f5b3b26ae060d6ff7f
Marlon-Schafranski <marlonschafranski963@gmail.com>
2023-11-24 12:00:03 -0300

    comit curriculo finalizado

--- src/components/intro/intro.jsx
+++ src/components/intro/intro.jsx
@@ -2,10 +2,34 @@ import React, { useState } from "react";
 import "./intro.css";
 import { Link } from "react-scroll";
 import btnImg from "../../assets/briefcase.png";
-import bg from "../../assets/kappa.png";
-import Hire from "../hire-me/hire"; // Importe o componente do formulário
+import bg from "../../assets/zyro-image2.png";
+import Hire from "../hire-me/hire";
+import { PDFDocument } from "pdf-lib";
+import curriculoPDF from "../../assets/curriculo.pdf"
+
 
 const Intro = () => {
+  const downloadPdf = async () => {
+    const curriculoBytes = await fetch(curriculoPDF).then((res) =>
+      res.arrayBuffer()
+    );
+
+    const pdfDoc = await PDFDocument.create();
+    const curriculoDoc = await PDFDocument.load(curriculoBytes);
+
+    const curriculoPages = await pdfDoc.copyPages(curriculoDoc, [0]);
+    pdfDoc.addPage(curriculoPages[0]);
+
+    const pdfBytes = await pdfDoc.save();
+    const pdfBlob = new Blob([pdfBytes], { type: "application/pdf" });
+    const pdfUrl = URL.createObjectURL(pdfBlob);
+    const link = document.createElement("a");
+    link.href = pdfUrl;
+    link.download = "documento.pdf";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
   const [showForm, setShowForm] = useState(false);
 
   const handleContactClick = () => {
@@ -22,8 +46,9 @@ const Intro = () => {
         <span className="ola"> Olá,</span>
         <span className="introText">
           {" "}
-          Eu sou o <span className="introName">Marlon Schafranski</span>{" "}
-          <br /> Desenvolvedor Web{" "}
+          Eu sou o <span className="introName">
+            Marlon Schafranski
+          </span> <br /> Desenvolvedor Web{" "}
         </span>
         <p className="introParagrafo">
           {" "}
@@ -36,8 +61,13 @@ const Intro = () => {
             <img src={btnImg} alt="Contrate-me" className="btnImg"></img>{" "}
             Contrate-me
           </button>
+          <div>
+            <button className="btn" onClick={downloadPdf}>
+              Baixar curriculo
+            </button>
+          </div>
         </Link>
-        {/* Mostra o formulário se showForm for verdadeiro */}
+
         {showForm && <Hire onClose={handleCloseForm} />}
       </div>
       <img src={bg} alt="photo" className="bg" />
